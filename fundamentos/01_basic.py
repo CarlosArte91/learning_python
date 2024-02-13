f0d1d6c09a636dc78936985a7c1876c1f4891f8b
carlos cruz <carturo99@gmail.com>
2024-02-13 18:46:52 -0500

    Fundaments

---
+++ fundamentos/01_basic.py
@@ -0,0 +1,42 @@
+# Hello world
+print("Hello world!\n\n")
+
+
+""" Operaciones básicas en programación con Python. """
+print("Operaciones básicas\n")
+
+# Suma
+print("La suma es =>", 1 + 3)
+
+# Resta
+print("La resta es =>", 55 - 28)
+
+# Multiplicación
+print("La multiplicación es =>", 6 * 9)
+
+# División
+print("La división es =>", 78 / 6)
+
+# Potencia o exponencial
+print("La potencia es =>", 5 ** 3)
+
+#  Modulo o restante
+print("El módulo es =>", 3 % 2)
+
+# División exacta (El resultado es un entero)
+print("La división exacta es =>", 25 // 6, "\n\n")
+
+
+""" Typos de datos en Python """
+print("Typos de datos\n")
+
+print("10 =>", type(10))
+print("3.1416 =>", type(3.1416))
+print("1 + 3j =>", type(1 + 3j))
+print("Hello python =>", type("Hello python"))
+print("True =>", type(True))
+print("False =>", type(False))
+print("[1, 2, 3] =>", type([1, 2, 3]))
+print("{'first_name': 'Carlos'} =>", type({'first_name': 'Carlos'}))
+print("{9.8, 3.14, 6.24} =>", type({9.8, 3.14, 6.24}))
+print("(9.8, 3.14, 6.24) =>", type((9.8, 3.14, 6.24)))
---
+++ fundamentos/02_variables.py
@@ -0,0 +1,35 @@
+""" Variable en Python """
+
+first_name = "Carlos"
+last_name = "Cruz"
+age = 33
+amount = 253.65
+is_active = True
+languajes = ["Python", "Java", "JavaScript", "Rust"]
+
+person_info = {
+  "first_name": first_name,
+  "last_name": last_name,
+  "age": age,
+  "country": "Colombia",
+}
+
+# Imprimiendo valores
+
+print("\nvariable first_name: ", first_name)
+print("Tipo de dato:", type(first_name), "\n")
+
+print("\nvariable age: ", age)
+print("Tipo de dato:", type(age), "\n")
+
+print("\nvariable amount: ", amount)
+print("Tipo de dato:", type(amount), "\n")
+
+print("\nvariable is_active: ", is_active)
+print("Tipo de dato:", type(is_active), "\n")
+
+print("\nvariable languajes: ", languajes)
+print("Tipo de dato:", type(languajes), "\n")
+
+print("\nvariable person_info: ", person_info)
+print("Tipo de dato:", type(person_info), "\n")
