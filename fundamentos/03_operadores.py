b2ca584ac2087ccb0f173d35d3c360ef108ee3d3
carlos cruz <carturo99@gmail.com>
2024-02-15 20:41:52 -0500

    Operadores

---
+++ fundamentos/03_operadores.py
@@ -0,0 +1,63 @@
+# Operadores en Python
+
+my_age = 33
+my_height = 1.75
+num_complex = 3j
+
+# # Calcular área del triangulo
+# print("############ ÁREA DEL TRIANGULO ##############\n")
+# base = int(input("Ingrese la base del triangulo: "))
+# height = int(input("Ingrese la altura del triangulo: "))
+
+# area_triangle = (base * height) / 2
+
+# print("\nEl área del triangulo es", area_triangle)
+
+
+# # Perímetro del triangulo
+# print("\n\n############ ÁREA DEL TRIANGULO ##############\n")
+# side_a = int(input("Ingrese el lado a del triangulo: "))
+# side_b = int(input("Ingrese el lado b del triangulo: "))
+# side_c = int(input("Ingrese el lado c del triangulo: "))
+
+# perimeter_triangle = sum([side_a, side_b, side_c])
+
+# print("\nEl perimetro del triangulo es", perimeter_triangle)
+
+
+# # Perímetro y área del rectangulo
+# print("\n\n############ PERIMETRO Y ÁREA DEL RECTANGULO ##############\n")
+# length = int(input("Ingrese el largo del rectangulo: "))
+# width = int(input("Ingrese el ancho del rectangulo: "))
+
+# perimeter_rectangle = length * 2 + width * 2
+# area_rectangle = length * width
+
+# print("\nEl perimetro del rectangulo es", perimeter_rectangle)
+# print("\nEl área del rectangulo es", area_rectangle)
+
+
+# # Circunferencia y área del circulo
+# print("\n\n############ CIRCUNFERENCIA Y ÁREA DEL CIRCULO ##############\n")
+# radius = float(input("Ingrese el radio del circulo: "))
+# pi = 3.1416
+
+# circunference = 2 * pi * radius
+# circle_area = pi * radius ** 2
+
+# print("\nLa circunferencia del circulo es", circunference)
+# print("\nEl área del circulo es", circle_area)
+
+
+# Comparación de variables
+name_a = 'python'
+name_b = 'dragon'
+
+print('python > dragon', len(name_a) > len(name_b))
+
+print('\nOn in python y dragon =>', 'on' in name_a and 'on' in name_b)
+
+frase = 'Espero que este curso no esté lleno de jerga'
+palabra = 'jerga'
+
+print('\nPalabra en Frase =>', palabra in frase)
